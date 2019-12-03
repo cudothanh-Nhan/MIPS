b00fa3ee3b341273d236be286949e7736dd7fd4b
thanhnhan <nhannk2001@gmail.com>
2019-12-03 11:12:09 +0700

    add getRegister.h

---
+++ getRegister.h
@@ -0,0 +1,22 @@
+#ifndef GET_REGISTER
+#define GET_REGISTER
+#include <string>
+using namespace std;
+string getRegister(string _str, int number) {
+    int countRegister = 0;
+    string word = "";
+    for(int i = 0; i <= _str.length() + 1; i++) {
+        if(_str[i] == '$') {
+            countRegister++;
+        }
+        if(countRegister == number) {         
+            while(_str[i] != ' ' && _str[i] != ',' && _str[i] != '\0' && _str[i] != ')' && _str[i] != '(') {
+                word += _str[i];
+                i++;
+            }
+            break;
+        }
+    }
+    return word; 
+}
+#endif
