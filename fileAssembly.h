d199a34947b0960daf4394f80f8d818da848b5be
thanhnhan <nhannk2001@gmail.com>
2019-12-06 19:17:58 +0700

    add Data.h to FileAssembly.h

--- fileAssembly.h
+++ fileAssembly.h
@@ -15,14 +15,23 @@ protected:
         int labelAddress = -1;
         FileAssembly::Label* next = nullptr;
     };
+    struct Data {
+        string name;
+        string type;
+        void* ptrData ;
+        Data* next = nullptr;
+    };
 	string linkFile;
 	string text[100];
 	int numberOfInstruction = 0;
-    Label* labelArray = nullptr;
+    Label* labelRoot = nullptr;
+    Data* dataRoot = nullptr;
 public:
     FileAssembly(string _linkFile);
 	string getInstruction(int);
     int getLabelAddress(string);
+    void* getDataAddress(string);
+    ~FileAssembly();
 };
 FileAssembly::FileAssembly(string _linkFile) : linkFile(_linkFile){
 // This is for TEXT PART
@@ -67,13 +76,13 @@ FileAssembly::FileAssembly(string _linkFile) : linkFile(_linkFile){
         if((int)offsetLine.find(":") != -1) {
 
             string label = getWord(offsetLine, 1);
-            if(labelArray == nullptr) {
-                labelArray = new Label;
-                labelArray->labelName = label;
-                labelArray->labelAddress = countAddress * 4;
+            if(labelRoot == nullptr) {
+                labelRoot = new Label;
+                labelRoot->labelName = label;
+                labelRoot->labelAddress = countAddress * 4;
             }
             else {
-                Label* seeker = labelArray;
+                Label* seeker = labelRoot;
                 while(seeker != nullptr) {
                     if(seeker->next == nullptr) {
                         seeker->next = new Label;
@@ -88,11 +97,63 @@ FileAssembly::FileAssembly(string _linkFile) : linkFile(_linkFile){
             if(word2.compare("")) countAddress++;
         }
     }
-    Label* seeker = labelArray;
+    Label* seeker = labelRoot;
     while(seeker != nullptr) {
         if(seeker->labelAddress > (countAddress - 1) * 4) seeker->labelAddress = -1;
         seeker = seeker->next;
     }
+// This is for DATA Part
+    fileIn.seekg(0);
+    string stringLine;
+    getline(fileIn, stringLine);
+    
+    while(!fileIn.eof()) {
+        getline(fileIn,stringLine);
+        if (!stringLine.compare(".text")) break;
+        
+        int countArray = 1;
+        for (int i = 3; i <= 50; i++) {
+            if (!getWord(stringLine,i).compare("")) break;
+            countArray++;
+        }
+        
+        Data* temp = dataRoot;
+        dataRoot = new Data;
+        
+        dataRoot->name = getWord(stringLine,1);
+        dataRoot->type = getWord(stringLine,2);
+        int countPtr = 0;
+
+        if (!getWord(stringLine,2).compare(".word") ) dataRoot->ptrData = (void*)(new int[countArray]);
+        else if (!getWord(stringLine,2).compare(".float")) dataRoot->ptrData = (void*)(new float[countArray]);
+        else if (!getWord(stringLine,2).compare(".double")) dataRoot->ptrData = (void*)(new double[countArray]);
+        else if (!getWord(stringLine,2).compare(".byte") || !getWord(stringLine,2).compare(".asciiz")) dataRoot->ptrData = (void*)(new string[countArray]);
+        
+        for (int i = 3 ; i <= 50 ; i++) {
+            if (!getWord(stringLine,i).compare("")) break;
+
+            if (!getWord(stringLine,2).compare(".word") ) {
+                *((int*)dataRoot->ptrData + countPtr) = stoi(getWord(stringLine,i));
+                countPtr++;
+            }
+            else if (!getWord(stringLine,2).compare(".float")) {
+                *((float*)dataRoot->ptrData + countPtr) = stof(getWord(stringLine,i));
+                countPtr++;
+            }
+            else if (!getWord(stringLine,2).compare(".double")) {
+                *((double*)dataRoot->ptrData + countPtr) = stod(getWord(stringLine,i));
+                countPtr++;
+            }
+            else if (!getWord(stringLine,2).compare(".byte")) {
+                *((string*)dataRoot->ptrData + countPtr) = getWord(stringLine,i);
+                countPtr++;
+            } 
+            else if (!getWord(stringLine,2).compare(".asciiz")) {
+                
+            }
+        }
+        dataRoot->next = temp;
+    } 
     fileIn.close();
 };
 string FileAssembly::getInstruction(int address){
@@ -101,12 +162,45 @@ string FileAssembly::getInstruction(int address){
 	else return "";
 }
 int FileAssembly::getLabelAddress(string _label) {
-    Label* seeker = labelArray;
+    Label* seeker = labelRoot;
     while(seeker != nullptr){
         if(!seeker->labelName.compare(_label)) return seeker->labelAddress;
         seeker = seeker->next; 
     }
     return -1;
+}
+void* FileAssembly::getDataAddress(string _name) {
+    Data* seeker = dataRoot;
+    while (seeker != nullptr) {
+        if (!seeker->name.compare(_name)) return seeker->ptrData;
+        seeker = seeker->next;
+    }
+}
+FileAssembly::~FileAssembly() {
+    Data* seeker1 = dataRoot;
+    while (seeker1 != nullptr) {
+        if (!seeker1->type.compare(".word")) {
+            delete[] (int*)seeker1->ptrData;
+        }
+        else if (!seeker1->type.compare(".float")) {
+            delete[] (float*)seeker1->ptrData;
+        }
+        else if (!seeker1->type.compare(".double")) {
+            delete[] (double*)seeker1->ptrData;
+        }
+        else if (!seeker1->type.compare(".byte") || !seeker1->type.compare(".asciiz")) {
+            delete[] (string*)seeker1->ptrData;
+        }
+        Data* temp = seeker1;
+        seeker1 = seeker1->next;
+        delete[] temp;
+    }
+    Label* seeker2 = labelRoot;
+    while(seeker2 != nullptr) {
+        Label* temp = seeker2;
+        seeker2 = seeker2->next;
+        delete[] temp;
+    }
 }
 // int main(){
 // 	FileAssembly input("assembly.txt");
