3603ee6b7aea347c87f5e0e486ffe4ccf9cd9c2b
An Thai Khac Duc <alphaan198@gmail.com>
2019-12-09 21:33:51 +0700

    fix hexa

--- getNumber.h
+++ getNumber.h
@@ -2,7 +2,7 @@
 #define GETNUMBER
 #include <iostream>
 #include <math.h>
-#include <string.h>
+#include <string>
 
 int getNumbers(string _instruction) {
 	string imm = "";
@@ -10,11 +10,20 @@ int getNumbers(string _instruction) {
 	int index = 0;
 	int count = 0;
 	bool dau = false;
+	bool hexa = false;
 	while (1) {
 		if (((int(_instruction[index]) >= 48)) && (int(_instruction[index] <= 57))) {
 			if ((_instruction[index - 1] == ' ') || (_instruction[index - 1] == ',') || (_instruction[index - 1] == '-')) {
 				if (_instruction[index - 1] == '-') dau = true;
 				while (1) {
+					if (_instruction[index+count + 1] = 'x'){
+						while (_instruction[index + count] != '\0'){
+							imm += (_instruction[index + count]);
+							count++;
+						}
+						hexa = true;
+						break;
+					}
 					imm  += _instruction[index + count];
 					count++;
 					if ((_instruction[index + count] == '\0') || (_instruction[index + count] == ' ') 
@@ -22,12 +31,11 @@ int getNumbers(string _instruction) {
 				}
 			}
 		}
-
 		if ((count > 0) || (_instruction[index + 1] == '\0')) break;
-
 		index++;
 	}
 	if(!imm.compare("")) return 0;
+	else if (hexa == true) return stoi(imm, 0, 16);
 	else return stoi(imm);
 }
 #endif
