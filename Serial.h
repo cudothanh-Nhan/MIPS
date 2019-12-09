7088f09e9fda24b8bfc0622f564e5429f5f97088
An Thai Khac Duc <alphaan198@gmail.com>
2019-12-09 22:13:00 +0700

    coproc1 added

--- Serial.h
+++ Serial.h
@@ -9,35 +9,48 @@ using namespace std;
 static bool isExit = 0;
 class Serial {
     int registerValue[35];
+    int coproc1Value[32];
 public:
     const char* registerName[35] {"$zero", "$at", "$v0", "$v1", "$a0", "$a1","$a2", "$a3", "$t0", "$t1", "$t2", "$t3", "$t4", "$t5", "$t6", "$t7",
     "$s0", "$s1", "$s2", "$s3", "$s4", "$s5", "$s6", "$s7", "$t8", "$t9", "$k0", "$k1", "$gp", "$sp", "$fp", "$ra", "pc", "hi", "lo"};
+    const char* coproc1Name[32] {"$f0", "$f1", "$f2", "$f3", "$f4", "$f5","$f6", "$f7", "$f8", "$f9", "$f10", "$f11", "$f12", "$f13", "$f14", "$f15",
+    "$f16", "$f17", "$f18", "$f19", "$f20", "$f21", "$f22", "$f23", "$f24", "$f25", "$f26", "$f27", "$f28", "$f29", "$f30", "$f31"};
     Serial();
     void print();
-    int write(string, int);
+    int write(string, double);
     void printCurrentLine(int, int);
     void pause();
 };
 
 Serial::Serial(){
     for (int i = 0; i < 35; i++) registerValue[i] = 0;
+    for (int i = 0; i < 32; i++) coproc1Value[i] = 0;
 }
 void Serial::print(){
     system("cls");
-    cout << setw(18) << left << "   Register Name" << "|" << setw(18) << left << "  Register Number" << " |" << setw(18) << " Value" << endl;
-    cout << "----------------------------------------------" << endl;
+    cout << "                   REGISTER                                           COPROCESSOR 1" << endl << endl;
+    cout << setw(18) << left << "   Register Name" << "|" << setw(18) << left << "  Register Number" << " |" << setw(18) << " Value"
+    << setw(19) << "  Name" << "|" << setw(18) << " Float" << " |" << setw(18) << " Double"<< endl;
+    cout << "----------------------------------------------          ---------------------------------------------------" << endl;
     for (int i = 0; i < 32; i++) {
-        cout << "  "<< left << setw(16) << registerName[i] << "| " << left << setw(18) << i << "| " << left << setw(18) << registerValue[i] << endl;
+        cout << "  "<< left << setw(16) << registerName[i] << "| " << left << setw(18) << i << "| " << left << setw(18) << registerValue[i] 
+        << left << setw(18) << coproc1Name[i] << "| " << left << setw(18) << float(coproc1Value[i]) << "| " << left << setw(18) << coproc1Value[i] << endl;
     }
     for (int i = 32; i < 35; i++) {
         cout << "  " << left << setw(16) << registerName[i] << "|                   " << "| " << setw(18) << registerValue[i] << endl;
     }
-    cout << "----------------------------------------------" << endl;
+    cout << "-----------------------------------------------------------------------------------------------------------" << endl;
 }
-int Serial::write(string registerInput, int valueInput){
+int Serial::write(string Input, double valueInput){
     for (int i = 0; i < 35; i++){
-        if (!registerInput.compare(registerName[i])){
-            registerValue[i] = valueInput;
+        if (!Input.compare(registerName[i])){
+            registerValue[i] = int(valueInput);
+            return 1;
+        }
+    }
+    for (int i = 0; i < 32; i++){
+        if (!Input.compare(coproc1Name[i])){
+            coproc1Value[i] = valueInput;
             return 1;
         }
     }
