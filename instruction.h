fa380e4955052cacfbd019f57278bbbd9a775178
thanhnhan <nhannk2001@gmail.com>
2019-12-09 19:11:14 +0700

    nearly completed

--- instruction.h
+++ instruction.h
@@ -1,3 +1,5 @@
+#ifndef INSTRUCTION
+#define INSTRUCTION
 #include <iostream>
 #include <iomanip>
 #include <string.h>
@@ -7,10 +9,7 @@
 #include "getNumber.h"
 #include "register.h"
 #include "fileAssembly.h"
-
-using namespace std;
-// PROTOTYPE----------------------------------------------------------
-// Regular Expression
+#pragma region INSTRUCTION_INTERFACE
 class System {
 public:
     static string consoleField;
@@ -35,7 +34,6 @@ void System::execute() {
             break;
     }
 }
-#pragma region INSTRUCTION_INTERFACE
 class Instruction {
 protected:
 public:
@@ -46,7 +44,6 @@ public:
     virtual ~Instruction(){}
 };
 #pragma endregion END
-
 #pragma region FORMAT_INTERFACE
 class R_Format : public Instruction {
 protected:
@@ -124,7 +121,6 @@ void J_Format::init(string _instruction){
 }
 
 #pragma endregion END
-
 #pragma region R-Fomat command Interface
 // R Format ----------------------------------------
 class Add : public R_Format {
@@ -621,70 +617,4 @@ void Jal::execute(){
 }
 Jal::~Jal(){};
 #pragma endregion J-Format Command Implementation
-
-
-Instruction* navigationCommand(string _instruction){
-    string name = getWord(_instruction, 1);
-    if(!name.compare("add")) return new Add;
-    else if(!name.compare("subtract")) return new Subtract;
-    else if(!name.compare("and")) return new And;
-    else if(!name.compare("sll")) return new Sll;
-    else if(!name.compare("mult")) return new Mult;
-    else if(!name.compare("div")) return new Div;
-    else if(!name.compare("jr")) return new Jr;
-    else if(!name.compare("mfhi")) return new Mfhi;
-    else if(!name.compare("mflo")) return new Mflo;
-    else if(!name.compare("sltu")) return new Sltu;
-    else if(!name.compare("addi")) return new Addi;
-    else if(!name.compare("andi")) return new Andi;
-    else if(!name.compare("ori")) return new Ori;
-	else if(!name.compare("slti")) return new Slti;	
-	else if(!name.compare("li")) return new Li;
-    else if(!name.compare("beq")) return new Beq;
-    else if(!name.compare("bne")) return new Bne;
-    else if(!name.compare("la")) return new La;
-    else if(!name.compare("lw")) return new Lw;
-    else if(!name.compare("sw")) return new Sw;
-    else if(!name.compare("lh")) return new Lh;
-    else if(!name.compare("j")) return new Jump;
-    else if(!name.compare("jal")) return new Jal;
-    else if(!name.compare("syscall")) {
-        sys.execute();
-        return nullptr;
-    }
-    else return nullptr;
-}
-void setup() {
-    cmd.print();
-    reg.init();
-}
-
-// Replace int main() with int process()
-int main(){
-    setup();
-    
-    //FileAssembly fileIn("testAssembly.txt");
-    while(fileIn.getInstruction(reg.getRegisterValue("pc")).compare("")) {
-        string instruction = fileIn.getInstruction(reg.getRegisterValue("pc"));
-        Instruction* ptr = navigationCommand(instruction);
-        cout << "------------------------------------------------------" <<'\n';
-        cout << "Next Command: " << optimizeString(instruction) << '\n';
-        cout << "------------------------------------------------------" << '\n';
-
-        if(ptr != nullptr) {
-            ptr->init(instruction);
-            ptr->execute();
-        }
-        reg.setRegisterValue("pc", reg.getRegisterValue("pc") + 4);
-        if(fileIn.getInstruction(reg.getRegisterValue("pc")).compare("")) {
-            cmd.write("pc", reg.getRegisterValue("pc"));
-        }
-        cmd.pause();
-        if(isExit == 1) break;
-        cmd.print();
-        cout << sys.consoleField << '\n';
-    }    
-    cout << "------------------------------------------------------" << '\n';
-    cout << "PROGRAM HAS ENDED!!" << '\n';
-    cout << "------------------------------------------------------" << '\n';
-}
+#endif
---
+++ main.cpp
@@ -0,0 +1,69 @@
+#include "instruction.h"
+
+using namespace std;
+
+Instruction* navigationCommand(string _instruction){
+    string name = getWord(_instruction, 1);
+    if(!name.compare("add")) return new Add;
+    else if(!name.compare("subtract")) return new Subtract;
+    else if(!name.compare("and")) return new And;
+    else if(!name.compare("sll")) return new Sll;
+    else if(!name.compare("mult")) return new Mult;
+    else if(!name.compare("div")) return new Div;
+    else if(!name.compare("jr")) return new Jr;
+    else if(!name.compare("mfhi")) return new Mfhi;
+    else if(!name.compare("mflo")) return new Mflo;
+    else if(!name.compare("sltu")) return new Sltu;
+    else if(!name.compare("addi")) return new Addi;
+    else if(!name.compare("andi")) return new Andi;
+    else if(!name.compare("ori")) return new Ori;
+	else if(!name.compare("slti")) return new Slti;	
+	else if(!name.compare("li")) return new Li;
+    else if(!name.compare("beq")) return new Beq;
+    else if(!name.compare("bne")) return new Bne;
+    else if(!name.compare("la")) return new La;
+    else if(!name.compare("lw")) return new Lw;
+    else if(!name.compare("sw")) return new Sw;
+    else if(!name.compare("lh")) return new Lh;
+    else if(!name.compare("j")) return new Jump;
+    else if(!name.compare("jal")) return new Jal;
+    else if(!name.compare("syscall")) {
+        sys.execute();
+        return nullptr;
+    }
+    else return nullptr;
+}
+void setup() {
+    cmd.print();
+    reg.init();
+}
+
+// Replace int main() with int process()
+int main(){
+    setup();
+    
+    //FileAssembly fileIn("testAssembly.txt");
+    while(fileIn.getInstruction(reg.getRegisterValue("pc")).compare("")) {
+        string instruction = fileIn.getInstruction(reg.getRegisterValue("pc"));
+        Instruction* ptr = navigationCommand(instruction);
+        cout << "------------------------------------------------------" <<'\n';
+        cout << "Next Command: " << optimizeString(instruction) << '\n';
+        cout << "------------------------------------------------------" << '\n';
+
+        if(ptr != nullptr) {
+            ptr->init(instruction);
+            ptr->execute();
+        }
+        reg.setRegisterValue("pc", reg.getRegisterValue("pc") + 4);
+        if(fileIn.getInstruction(reg.getRegisterValue("pc")).compare("")) {
+            cmd.write("pc", reg.getRegisterValue("pc"));
+        }
+        cmd.pause();
+        if(isExit == 1) break;
+        cmd.print();
+        cout << sys.consoleField << '\n';
+    }    
+    cout << "------------------------------------------------------" << '\n';
+    cout << "PROGRAM HAS ENDED!!" << '\n';
+    cout << "------------------------------------------------------" << '\n';
+}
